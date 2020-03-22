cd1bf761e00dee5cebfc164909b4951b9b21ebb3
kirakbol <www.kiriakosbolos@gmail.com>
2020-03-22 15:28:27 +0200

    added the watchdog package to view an activity log for the actions made on the directory the script is in

--- organizer.py
+++ organizer.py
@@ -1,6 +1,12 @@
 import os
 from pathlib import Path
 
+import sys
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+
 
 fileList = []
 
@@ -12,11 +18,21 @@ def files(path):
                 os.rename(file, '.' + file)
     return fileList
 
-def main():
-    print(files('.'))
-
-main()
 
 
-
-
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    files(path)
+    event_handler = LoggingEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
